13168d07a8c388f765c3fb0f425e15fa4a50d5a9
MarCrivelli <marcoantonio7crivelli@gmail.com>
2023-09-14 14:28:00 -0400

    Update 2
    card adicionado

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3010;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { message: 'Lojinha do Crivelli' });
+});
+
+app.get('/contato', (req, res) => {
+  res.render('contato', { message: 'Oi, tudo bem?' });
 });
 
 app.listen(port, () => {
